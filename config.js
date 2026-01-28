a7c4ed882720366a9d67089524ff4ac5689bc897
sarifuddinmondal <sarifuddinm9@gmail.com>
2026-01-28 13:27:05 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// CKART Global Configuration
+const CONFIG = {
+    USER: "sarifuddinmondal",
+    REPO: "ckart-project",
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbx2o3DoJJqSnzDXYLkBu_rwxZCCnCAkxEXRI0U-1b93e5ZRwPtNx6xI-GPQ7QTpWoK0/exec",
+    // এই URL পরিবর্তন করলে সব পেজে অটোমেটিক আপডেট হয়ে যাবে
+};
