9b313183e2cdd1cccd3d2abbfa4aba462775bf1e
sarifuddinmondal <sarifuddinm9@gmail.com>
2026-01-29 10:32:01 +0530

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// CKART Global Configuration
-const CONFIG = {
-    USER: "sarifuddinmondal",
-    REPO: "ckart-project",
-    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwQMgx6GCTbI_GWCGWzntuuZhUTQWWROZuZ4aGn6Xb1q6J-9InDYC9gyi3_4U5ERsyj/exec",
-    // এই URL পরিবর্তন করলে সব পেজে অটোমেটিক আপডেট হয়ে যাবে
-};
